51c41f9cf75a4e906c1047c2a9ee978b592bd840
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2025-10-15 00:01:27 +0200

    update tests with better description string
    - and trim / tweak tests to focus on what's important to validate the
      correctness of the solver.
    
    - TODO: their still remains more work to do on this

--- tests/srcs/test_solver.cpp
+++ tests/srcs/test_solver.cpp
@@ -10,32 +10,40 @@ using std::endl;
 
 // Struct for a single test case
 struct Test {
+    std::string description;                        // <--- Added description
     std::string ruleSet;                             // input rules/facts/queries
     std::unordered_map<char, Fact::State> expected;  // expected results for queries
 };
 
 // Helper to run one test
 void runTest(const Test &t) {
-
-
     auto tokens = tokenizer(t.ruleSet);
     auto [rules, facts, queries] = parseTokens(tokens);
 
     Digraph digraph = makeDigraph(facts, rules);
     digraph.applyWorldAssumption(false);
 
+    bool failed = false;
+
     for (const auto &[label, expectedState] : t.expected) {
         auto res = digraph.solveForFact(label);
 
         if (res != expectedState) {
-            cout << "Running test with ruleset:\n" << t.ruleSet << endl
-                 << "Query " << label << " got " << res
+            if (!failed) {
+                cout << RED << "Test failed: " << RESET << t.description << endl;
+                cout << "Ruleset:\n" << t.ruleSet << endl;
+            }
+
+            cout << "Query " << label << " got " << res
                  << " (expected " << expectedState << ")\n"
                  << RED << "KO " << RESET << label << endl;
-        } else {
-            cout << GREEN << "OK" << RESET << endl;
+
+            failed = true;
         }
     }
+
+    if (!failed)
+        cout << GREEN << "OK" << RESET << endl;
 
     cout << "--------------------------------------\n";
 }
@@ -45,56 +53,81 @@ int main() {
 
     std::vector<Test> tests = {
         {
+            "Simple implication",
             "A=>B\n=A\n?B",
             { {'B', Fact::State::True} }
         },
         {
+            "OR in consequent: lhs as true",
             "A|B=>C\n=A\n?C",
             { {'C', Fact::State::True} }
         },
         {
+            "OR in consequen: rhs as true",
             "A|B=>C\n=B\n?C",
             { {'C', Fact::State::True} }
         },
         {
+            "Chained rules and ANDs",
             "C+E=>F\nH+S=>K\nF=>G\nK=>Y\n=CE\n?G",
             { {'G', Fact::State::True} }
         },
         {
-            "A=>B|C\n=A\n?C", // Undetermined example from the docs
+            "Or in conclusion: Undetermined example from docs",
+            "A=>B|C\n=A\n?C",
             { {'B', Fact::State::Undetermined}, {'C', Fact::State::Undetermined}}
         },
         {
-            "A|F=>B\n=A\n?F", // Closed world, F is not part of any implicaiton solution
+            "Closed world assumption: F is not part of any implication therefore it should be false",
+            "A|F=>B\n=A\n?F",
             { {'F', Fact::State::False}}
         },
         {
-            "A=>B\n=A\n?F", // Closed world, should be false
+            "Closed world assumption with unused F",
+            "A=>B\n=A\n?F",
             { {'F', Fact::State::False}}
         },
         {
-            "!A=>B\n=A\n?B", // should be False, because closed world,
-                             // no antecedent is true that
-                             // could prove/disprove/undecide the fact, is this correct?
+            "Negation in rule: Should yield false becasue closed world and no antecedent \n\
+             is true that could prove/disprove/undecide the fact, (is this correct?)",
+            "!A=>B\n=A\n?B",
             { {'B', Fact::State::False} }
         },
         {
-            "A|F=>B\n=A\n?F", // Closed world, should be false
-            { {'F', Fact::State::False}}
-        },
-        {
-            "A=>B^C\n=A\n?B", // 
-            { {'B', Fact::State::Undetermined}}
-        },
-        {
-            "A=>!B\n=A\n?B", // Netation in conclusion
+            "Negation in conclusion: A=>!B, B should be false",
+            "A=>!B\n=A\n?B",
             { {'B', Fact::State::False}}
         },
         {
-            "A=>B^C\nA=>!B\n=A\n?C", // Xor in conclusion,
-                                     // C must be true, because A is true and B is false
+            "XOR in conclusion, with hls as true",
+            "A=>B^C\n=AB\n?BC",
+            { {'B', Fact::State::True}, {'C', Fact::State::False}}
+        },
+        {
+            "XOR in conclusion with rhs as negative",
+            "A=>B^C\nA=>!B\n=A\n?C",
             { {'A', Fact::State::True}, {'B', Fact::State::False}, {'C', Fact::State::True}}
-        }
+        },
+        {
+            "XOR and NOT combined in conclusion",
+            "A=>B^!C\n=AB\n?BC",
+            { {'B', Fact::State::True}, {'C', Fact::State::True}}
+        },
+        {
+            "AND in conclusion",
+            "A=>B+C\n=A\n?BC",
+            { {'B', Fact::State::True}, {'C', Fact::State::True}}
+        },
+        {
+            "Compound AND in conclusion",
+            "A=>B+C+D\n=A\n?BCD",
+            { {'B', Fact::State::True}, {'C', Fact::State::True}, {'D', Fact::State::True}}
+        },
+        {
+            "Combining AND with NOT in a conclusion",
+            "A=>B+!C\n=A\n?BC",
+            { {'B', Fact::State::True}, {'C', Fact::State::False}}
+        },
     };
 
     for (const auto &t : tests) {
